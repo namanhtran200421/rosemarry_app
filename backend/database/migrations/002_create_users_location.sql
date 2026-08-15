7e6060565af18c88557a422b2751fa878c7f9794
snich1234 <nicholassamuel223@gmail.com>
2026-08-15 19:48:25 +1000

    users location migration file

---
+++ backend/database/migrations/002_create_users_location.sql
@@ -0,0 +1,18 @@
+--migrate:up
+create table users_location (
+    user_id integer not null,
+    latitude numeric(9,6) not null,
+    longitude numeric (9,6) not null,
+    postcode text,
+    city varchar(100),
+    state varchar(100),
+    country VARCHAR(100),
+    location_updated_at timestamp not null default now(),
+
+    constraint pk_users_location primary key (user id),
+    constraint fk_users_location_user
+        foreign key (user_id) references users (user_id)
+        on delete cascade
+)
+--migrate:down
+drop table if exists users_location;
