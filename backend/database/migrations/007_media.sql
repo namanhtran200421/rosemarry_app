47713e7f078b05401f5d08d1fc1928df25558b7f
Nam Anh Tran <steve200994@gmail.com>
2026-08-16 19:51:29 +1000

    add migration files for profile_photos and medias tables
    - ticket:
    SCRUM-37

---
+++ backend/database/migrations/007_media.sql
@@ -0,0 +1,14 @@
+--migrate:up
+create table medias (
+    media_id int generated always as identity,
+    media_url text not null, 
+    mime_type varchar(100) not null, 
+    file_size_bytes bigint not null, 
+    created_at timestamptz not null default now(),  
+
+    constraint pk_media primary key (media_id)
+    constraint chk_media_file_size check (file_size_bytes >= 0)  
+);
+
+--migrate:down
+drop table if EXISTS medias;
