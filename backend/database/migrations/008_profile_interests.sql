8f7b461080e970852e03c3e26c82e888bb4b96ab
steve <namanhtran587@gmail.com>
2026-08-22 10:08:24 +1000

    Add foreign key constraints to profile_interests table

--- backend/database/migrations/008_profile_interests.sql
+++ backend/database/migrations/008_profile_interests.sql
@@ -5,6 +5,11 @@ create table profile_interests(
     created_at timestampz not null default now(),
 
     constraint pk_profile_interests primary key(user_id, interest_id)
+    constraint fk_user_id foreign key (user_id) references profiles(user_id)
+    on delete cascade,
+
+    constraint fk_interest_id foreign key (interest_id) references interests(interest_id)
+    on delete cascade, 
 );
 
 -- migrate:down
