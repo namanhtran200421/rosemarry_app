6aaa8891f2a2c530b0cceb82c9bb5eaa48ee6e11
steve <namanhtran587@gmail.com>
2026-08-22 10:09:28 +1000

    Fix timestamp type in profile_interests migration

--- backend/database/migrations/008_profile_interests.sql
+++ backend/database/migrations/008_profile_interests.sql
@@ -2,7 +2,7 @@
 create table profile_interests(
     user_id integer generated always as identity, 
     interest_id integer generated always as identity,
-    created_at timestampz not null default now(),
+    created_at timestamptz not null default now(),
 
     constraint pk_profile_interests primary key(user_id, interest_id)
     constraint fk_user_id foreign key (user_id) references profiles(user_id)
