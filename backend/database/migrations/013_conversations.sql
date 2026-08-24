f238281afe71723c1023e804750232015b4268aa
snich1234 <nicholassamuel223@gmail.com>
2026-08-24 21:52:08 +1000

    Added DDL for conversations

---
+++ backend/database/migrations/013_conversations.sql
@@ -0,0 +1,23 @@
+--migrate:up
+
+CREATE TYPE conversation_type_enum AS ENUM ('direct', 'circle');
+
+CREATE TABLE conversations(
+    conversation_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    type conversation_type_enum NOT NULL,
+    cycle_id int REFERENCES circle_cycles (cycle_id) ON DELETE CASCADE,
+    dm_key varchar(64),
+    last_message_at timestamptz,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+
+    -- Circles have cycles and cycle IDs, private messages have DM keys
+    CONSTRAINT conversations_shape CHECK (
+        (type = 'circle' AND cycle_id IS NOT NULL AND dm_key IS NULL)
+        OR (type = 'direct' AND cycle_id IS NULL AND dm_key IS NOT NULL)
+    )
+);
+
+--migrate:down
+drop table if exists conversations;
+drop type if exists conversation_type_enum;
