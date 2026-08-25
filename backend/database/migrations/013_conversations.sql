8bf9086cc175dd22acf7432de294f914a7232eee
steve <namanhtran587@gmail.com>
2026-08-25 12:49:48 +1000

    Fix syntax error in conversations SQL migration
    i miss a comma

--- backend/database/migrations/013_conversations.sql
+++ backend/database/migrations/013_conversations.sql
@@ -15,7 +15,7 @@ CREATE TABLE conversations(
     CONSTRAINT conversations_shape CHECK (
         (type = 'circle' AND cycle_id IS NOT NULL AND dm_key IS NULL)
         OR (type = 'direct' AND cycle_id IS NULL AND dm_key IS NOT NULL)
-    )
+    ),
 
     CONSTRAINT conversations_dm_key_unique UNIQUE (dm_key),
 );
